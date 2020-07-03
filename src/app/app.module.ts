78267564f7674658c7db15e34a0e104cf8c7051a
LeonardoGarza <leonardo.garza_18@outlook.com>
2020-07-03 16:33:41 -0500

    E2 A4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { VistasComponent } from './vistas/vistas.component';
 import { ListComponent } from './list/list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Automovil } from '../models';
 import { AUTOMOVILES } from '../data'
 import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap"
 
 @Component({
   selector: 'app-list',
@@ -10,11 +11,33 @@ import { faEye } from '@fortawesome/free-solid-svg-icons';
 })
 export class ListComponent implements OnInit {
   autos: Automovil[];
+  autoSeleccionado: Automovil;
+
+  closeResult = '';
   faEye = faEye;
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.autos = AUTOMOVILES;
+  }
+
+  onSelect(auto: Automovil, content){
+    this.autoSeleccionado = auto;
+    this.modalService.open(content, {ariaLabelledBy: 'Detalles'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
